41da604a86fcbfcbf2c3d767d7d708b49061f00c
Mikael Mortensen <mikaem@math.uio.no>
2013-08-20 11:11:04 +0200

    Adding solution

--- TaylorGreen2D.py
+++ TaylorGreen2D.py
@@ -6,31 +6,7 @@ __license__  = "GNU Lesser GPL version 3 or any later version"
 from Oasis import *
 
 # Create a mesh here
-mesh = UnitSquareMesh(4, 4)
-scale = 2.*mesh.coordinates()
-mesh.coordinates()[:, :] = scale
-del scale
-
-class PeriodicDomain(SubDomain):
-    
-    def inside(self, x, on_boundary):
-        # return True if on left or bottom boundary AND NOT on one of the two corners (0, 1) and (1, 0)
-        return bool((near(x[0], 0) or near(x[1], 0)) and 
-                (not ((near(x[0], 0) and near(x[1], 2)) or 
-                        (near(x[0], 2) and near(x[1], 0)))) and on_boundary)
-
-    def map(self, x, y):
-        if near(x[0], 2) and near(x[1], 2):
-            y[0] = x[0] - 2.0
-            y[1] = x[1] - 2.0
-        elif near(x[0], 2):
-            y[0] = x[0] - 2.0
-            y[1] = x[1]
-        else: #near(x[1], 2)
-            y[0] = x[0]
-            y[1] = x[1] - 2.0
-
-constrained_domain = PeriodicDomain()
+mesh = RectangleMesh(0, 0, 2, 2, 20, 20)
 
 # Override some problem specific parameters
 NS_parameters.update(dict(
@@ -43,20 +19,33 @@ NS_parameters.update(dict(
     convection = "Standard",
     use_krylov_solvers = False,
     use_lumping_of_mass_matrix = True,
-    velocity_degree = 7,
-    pressure_degree = 6,
+    velocity_degree = 2,
+    pressure_degree = 1,
     monitor_convergence = False,
     krylov_report = False    
   )
 )
 
-def pre_solve_hook(q_, p_, **NS_namespace):    
-    velocity_plotter0 = VTKPlotter(q_['u0'])
-    velocity_plotter1 = VTKPlotter(q_['u1'])
-    pressure_plotter = VTKPlotter(p_) 
-    return dict(velocity_plotter0=velocity_plotter0, 
-                velocity_plotter1=velocity_plotter1, 
-                pressure_plotter=pressure_plotter)
+class PeriodicDomain(SubDomain):
+    
+    def inside(self, x, on_boundary):
+        # return True if on left or bottom boundary AND NOT on one of the two corners (0, 1) and (1, 0)
+        return bool((near(x[0], 0) or near(x[1], 0)) and 
+              (not ((near(x[0], 0) and near(x[1], 2)) or 
+                    (near(x[0], 2) and near(x[1], 0)))) and on_boundary)
+
+    def map(self, x, y):
+        if near(x[0], 2) and near(x[1], 2):
+            y[0] = x[0] - 2.0
+            y[1] = x[1] - 2.0
+        elif near(x[0], 2):
+            y[0] = x[0] - 2.0
+            y[1] = x[1]
+        else:
+            y[0] = x[0]
+            y[1] = x[1] - 2.0
+
+constrained_domain = PeriodicDomain()
 
 initial_fields = dict(
     u0='-sin(pi*x[1])*cos(pi*x[0])*exp(-2.*pi*pi*nu*t)',
@@ -72,11 +61,10 @@ def initialize(q_, q_1, q_2, VV, t, nu, dt, **NS_namespace):
             q_1[ui].vector()[:] = q_[ui].vector()[:]
             q_2[ui].vector()[:] = q_[ui].vector()[:]
 
-def temporal_hook(q_, t, nu, VV, dt, velocity_plotter0, velocity_plotter1,
-                           pressure_plotter, **NS_namespace):
-    pressure_plotter.plot()
-    velocity_plotter0.plot()
-    velocity_plotter1.plot()
+def temporal_hook(q_, t, nu, VV, dt, **NS_namespace):
+    plot(q_['u0'], title='u')
+    plot(q_['u1'], title='v')
+    plot(q_['p'], title='p')
     err = {}
     for ui in q_.keys():
         deltat = dt/2. if ui is 'p' else 0.
