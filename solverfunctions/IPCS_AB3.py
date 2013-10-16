17dba51eb79ea47b95e908d34637fd7814a17a9a
Mikael Mortensen <mikaem@math.uio.no>
2013-10-16 11:17:47 +0200

    Adding new segregated solver IPCS_AB3

---
+++ solverfunctions/IPCS_AB3.py
@@ -0,0 +1,6 @@
+__author__ = "Mikael Mortensen <mikaem@math.uio.no>"
+__date__ = "2013-10-14"
+__copyright__ = "Copyright (C) 2013 " + __author__
+__license__  = "GNU Lesser GPL version 3 or any later version"
+
+from IPCS_AB2 import *
