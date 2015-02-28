b80a8a04696ffe86ca168f2a56ae3629c93996ef
Mikael Mortensen <mikaem@math.uio.no>
2015-02-28 07:22:07 -0800

    Adding linear solver to KinteticEnergySGS

--- solvers/NSfracStep/LES/KineticEnergySGS.py
+++ solvers/NSfracStep/LES/KineticEnergySGS.py
@@ -3,9 +3,8 @@ __date__ = '2015-02-24'
 __copyright__ = 'Copyright (C) 2015 ' + __author__
 __license__  = 'GNU Lesser GPL version 3 or any later version'
 
-from dolfin import Function, FunctionSpace, assemble, TestFunction, sym, grad,\
-        dx, inner, as_backend_type, TrialFunction, project, CellVolume, sqrt,\
-        solve, dot, lhs, rhs, interpolate, Constant, DirichletBC, FacetFunction
+from dolfin import Function, FunctionSpace, assemble, TestFunction, sym, grad, Vector,\
+    dx, inner, TrialFunction, sqrt, Matrix, dot, interpolate, Constant, DirichletBC, KrylovSolver
 
 from common import derived_bcs
 
@@ -32,11 +31,17 @@ def les_setup(u_, mesh, KineticEnergySGS, assemble_matrix, CG1Function, nut_kryl
     nut_form = Ck * delta * sqrt(ksgs)
     bcs_nut = derived_bcs(CG1, bcs['u0'], u_)
     nut_ = CG1Function(nut_form, mesh, method=nut_krylov_solver, bcs=bcs_nut, bounded=True, name="nut")
+    At = Matrix()
+    bt = Vector(nut_.vector())
+    ksgs_sol = KrylovSolver("bicgstab", "additive_schwarz")
+    ksgs_sol.parameters["preconditioner"]["structure"] = "same_nonzero_pattern"
+    ksgs_sol.parameters["error_on_nonconvergence"] = False
+    ksgs_sol.parameters["monitor_convergence"] = False
+    ksgs_sol.parameters["report"] = False
+    del NS_namespace
+    return locals()
 
-    return dict(nut_form=nut_form, nut_=nut_, delta=delta, ksgs=ksgs,
-                CG1=CG1, A_mass=A_mass, bc_ksgs=bc_ksgs, bcs_nut=bcs_nut)    
-
-def les_update(nut_, nut_form, A_mass, u_, dt, bc_ksgs,
+def les_update(nut_, nut_form, A_mass, At, u_, dt, bc_ksgs, bt, ksgs_sol,
         KineticEnergySGS, CG1, ksgs, delta, **NS_namespace):
 
     p, q = TrialFunction(CG1), TestFunction(CG1)
@@ -45,15 +50,18 @@ def les_update(nut_, nut_form, A_mass, u_, dt, bc_ksgs,
     Ce = KineticEnergySGS["Ce"]
 
     Sij = sym(grad(u_))
-    A = assemble(dt*inner(dot(u_,0.5*grad(p)), q)*dx \
-            + inner((dt*Ce*sqrt(ksgs)/delta)*0.5*p,q)*dx \
-            + inner(dt*Ck*sqrt(ksgs)*delta*grad(0.5*p),grad(q))*dx)
-    b = A_mass*ksgs.vector() - A*ksgs.vector() + assemble(dt*2*Ck*delta*sqrt(ksgs)*inner(Sij,grad(u_))*q*dx)
-    A.axpy(1.0, A_mass, True)
+    assemble(dt*inner(dot(u_, 0.5*grad(p)), q)*dx
+             + inner((dt*Ce*sqrt(ksgs)/delta)*0.5*p, q)*dx
+             + inner(dt*Ck*sqrt(ksgs)*delta*grad(0.5*p), grad(q))*dx, tensor=At)
+    
+    assemble(dt*2*Ck*delta*sqrt(ksgs)*inner(Sij,grad(u_))*q*dx, tensor=bt)
+    bt.axpy(1.0, A_mass*ksgs.vector())
+    bt.axpy(-1.0, At*ksgs.vector())
+    At.axpy(1.0, A_mass, True)
 
     # Solve for ksgs
-    bc_ksgs.apply(A,b)
-    solve(A, ksgs.vector(), b, "bicgstab", "additive_schwarz")
+    bc_ksgs.apply(At, bt)
+    ksgs_sol.solve(At, ksgs.vector(), bt)
     ksgs.vector().set_local(ksgs.vector().array().clip(min=1e-7))
     ksgs.vector().apply("insert")
 
