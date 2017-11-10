9a7a12a6ed1b85e4ea9e52108e7f7c97563408b5
Mikael Mortensen <mikaem@math.uio.no>
2017-11-10 15:06:29 +0100

    Adding source_files to meta.yml

--- conf/conda/run_test.sh
+++ conf/conda/run_test.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-set -e
 
 if [[ "$(uname)" == "Darwin" ]]; then
   export MACOSX_DEPLOYMENT_TARGET=10.9
   export CXXFLAGS="-std=c++11 -stdlib=libc++ $CXXFLAGS"
 fi
 
-python -b -m pytest -vs tests/
+py.test tests/
 
-#mpirun -np 2 py.test -v
+#mpirun -np 2 py.test -v tests/
